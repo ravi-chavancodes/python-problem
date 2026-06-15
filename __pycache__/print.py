e040bcc4133b6628f7ffc21b466ae4b95408aed4
ravi-chavancode <ravisingh79471@gmail.com>
2026-06-15 19:33:10 +0530

    variabels and sum

---
+++ __pycache__/print.py
@@ -0,0 +1,17 @@
+# print 
+"""
+print("Hello World")
+print("Welcome to Python")
+print(10)
+print(10 + 20)
+"""
+
+# variable 
+
+name = "Ravi"
+age = 21
+height = 5.8
+
+print(name)
+print(age)
+print(height)
