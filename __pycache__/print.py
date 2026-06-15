d9e8fc0459884f25317ee167f04f97813c4d7313
ravi-chavancode <ravisingh79471@gmail.com>
2026-06-15 19:36:01 +0530

    input,operator

--- __pycache__/print.py
+++ __pycache__/print.py
@@ -7,7 +7,7 @@ print(10 + 20)
 """
 
 # variable 
-
+"""
 name = "Ravi"
 age = 21
 height = 5.8
@@ -15,3 +15,46 @@ height = 5.8
 print(name)
 print(age)
 print(height)
+
+"""
+
+# data type
+"""
+name = "Ravi"
+age = 21
+salary = 50000.50
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(is_student))
+
+"""
+
+# input
+
+"""
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+
+print("Name:", name)
+print("Age:", age)
+
+"""
+
+# operator
+
+"""
+
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a // b)
+"""
