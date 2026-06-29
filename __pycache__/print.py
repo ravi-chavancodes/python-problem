b6799c54c6ab59e9a0deb2c64d0a5626c7556b2b
ravi-chavancode <ravisingh79471@gmail.com>
2026-06-29 20:24:16 +0530

    all nested

--- __pycache__/print.py
+++ __pycache__/print.py
@@ -299,5 +299,248 @@ price = [100,200,300]
 for i in enumerate(price):
     print (i)
     """
+# Nested Loop
+
+"""
+for i in range(3):
+    for j in range(3):
+        print(i, j)
+"""
 
 
+# Print Characters of String
+
+"""
+name = "vedant"
+
+for i in name:
+    print(i)
+"""
+
+
+# Break
+
+"""
+name = "vedant"
+
+for i in name:
+    if i == 'a':
+        break
+    print(i)
+"""
+
+
+# Continue
+
+"""
+name = "vedant"
+
+for i in name:
+    if i == 'a':
+        continue
+    print(i)
+"""
+
+
+# Pass
+
+"""
+name = "vedant"
+
+for i in name:
+    if i == 'a':
+        pass
+    print(i)
+"""
+
+
+# Enumerate
+
+"""
+price = [100, 200, 300]
+
+for i in enumerate(price):
+    print(i)
+"""
+# Nested Loop
+# Used when one loop runs inside another loop.
+
+"""
+for i in range(3):
+    for j in range(3):
+        print(i, j)
+"""
+
+
+# Print Characters of String
+# Prints each character of the string one by one.
+
+"""
+name = "vedant"
+
+for i in name:
+    print(i)
+"""
+
+
+# Break
+# Stops the loop when the condition is true.
+
+"""
+name = "vedant"
+
+for i in name:
+    if i == 'a':
+        break
+    print(i)
+"""
+
+
+# Continue
+# Skips the current iteration when the condition is true.
+
+"""
+name = "vedant"
+
+for i in name:
+    if i == 'a':
+        continue
+    print(i)
+"""
+
+
+# Pass
+# Does nothing when the condition is true.
+
+"""
+name = "vedant"
+
+for i in name:
+    if i == 'a':
+        pass
+    print(i)
+"""
+
+
+# Enumerate
+# Prints both index and value of a list.
+
+"""
+price = [100, 200, 300]
+
+for i in enumerate(price):
+    print(i)
+"""
+
+
+# Enumerate (Index and Value)
+# Stores index and value in separate variables.
+
+"""
+price = [100, 200, 300]
+
+for index, value in enumerate(price):
+    print(index, value)
+"""
+
+
+# Square Pattern
+# Prints a square of stars using nested loops.
+
+"""
+for i in range(4):
+    for j in range(4):
+        print("*", end=" ")
+    print()
+"""
+
+
+# Rectangle Pattern
+# Prints a rectangle of stars.
+
+"""
+for i in range(3):
+    for j in range(5):
+        print("*", end=" ")
+    print()
+"""
+
+
+# Left Triangle
+# Prints a left-aligned triangle.
+
+"""
+for i in range(1, 6):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+"""
+
+
+# Inverted Triangle
+# Prints an upside-down triangle.
+
+"""
+for i in range(5, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+"""
+
+
+# Number Triangle
+# Prints numbers from 1 to the current row.
+
+"""
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+"""
+
+
+# Same Number Pattern
+# Prints the row number multiple times.
+
+"""
+for i in range(1, 6):
+    for j in range(i):
+        print(i, end=" ")
+    print()
+"""
+
+
+# Floyd's Triangle
+# Prints continuous numbers in triangle form.
+
+"""
+num = 1
+
+for i in range(1, 6):
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+    print()
+"""
+
+
+# Alphabet Triangle
+# Prints alphabets in a triangle pattern.
+
+"""
+for i in range(1, 5):
+    for j in range(i):
+        print(chr(65 + j), end=" ")
+    print()
+"""
+
+
+# Multiplication Table
+# Prints a multiplication table from 1 to 5.
+
+"""
+for i in range(1, 6):
+    for j in range(1, 6):
+        print(i * j, end="\t")
+    print()
+"""
