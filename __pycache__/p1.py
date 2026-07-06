ed34fb855454408ee4eb66bd87b85aa966c4b49b
ravi-chavancode <ravisingh79471@gmail.com>
2026-07-06 18:36:33 +0530

    exception

--- __pycache__/p1.py
+++ __pycache__/p1.py
@@ -158,3 +158,24 @@ data = file.read()
 print(data)
 file.close()
 """
+
+#8 Exception Handling – Handling Errors Using try, except, and finally
+
+"""
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    result = num1 / num2
+
+    print("Result =", result)
+
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
+
+except ValueError:
+    print("Error: Please enter valid numbers.")
+
+finally:
+    print("Program Executed Successfully.")
+"""
