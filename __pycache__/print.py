9e4bbbeba6ed372a032bc9359ee8ae9a35df4c80
ravi-chavancode <ravisingh79471@gmail.com>
2026-07-18 22:51:39 +0530

    todo

--- __pycache__/print.py
+++ __pycache__/print.py
@@ -649,3 +649,35 @@ while seconds > 0:
 print("Time's Up!")
 """
 
+#to-do list
+"""
+tasks = []
+
+while True:
+    print("\n1. Add Task")
+    print("2. View Tasks")
+    print("3. Remove Task")
+    print("4. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        task = input("Enter task: ")
+        tasks.append(task)
+
+    elif choice == "2":
+        print("\nTasks:")
+        for i, task in enumerate(tasks, start=1):
+            print(f"{i}. {task}")
+
+    elif choice == "3":
+        index = int(input("Enter task number: ")) - 1
+        if 0 <= index < len(tasks):
+            tasks.pop(index)
+
+    elif choice == "4":
+        break
+
+    else:
+        print("Invalid choice.")
+        """
