cdb5d152b1eab4f3f19123e56350d3101ac17e3b
ravi-chavancode <ravisingh79471@gmail.com>
2026-08-06 17:45:16 +0530

    ee

--- __pycache__/p1.py
+++ __pycache__/p1.py
@@ -335,3 +335,14 @@ file.close()
 # Aim: To write a Python program to take user input and save it into a text file.
 
 # Program:
+"""
+text = input("Enter text: ")
+
+file = open("output.txt", "w").    
+
+file.write(text)
+
+file.close()
+
+print("Data saved successfully.")
+"""
