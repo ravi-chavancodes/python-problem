eb8949e7211be76936a820bd27efe2047a5a69f5
ravi-chavancode <ravisingh79471@gmail.com>
2026-08-13 15:27:29 +0530

    ee

--- __pycache__/p1.py
+++ __pycache__/p1.py
@@ -1,5 +1,20 @@
 # Hello World Program
+"""
+print("Hello, World!")
 
+# Basic Arithmetic Operations
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("Floor Division =", a // b)
+print("Modulus =", a % b)
+print("Exponent =", a ** b)
+"""
 
 # Practical No. 2
 
