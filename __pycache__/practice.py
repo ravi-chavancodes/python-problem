037ccfbff1bc9807c5a745defebc1f9937334f83
ravi-chavancode <ravisingh79471@gmail.com>
2026-08-16 13:45:18 +0530

    ee

---
+++ __pycache__/practice.py
@@ -0,0 +1,14 @@
+"""
+def is_palindrome(text):
+
+    return text == text[::-1]
+
+
+word = input("Enter a word: ").lower()
+
+if is_palindrome(word):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+    """
+
